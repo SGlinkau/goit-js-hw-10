cf61fbc8d1628f93efb35856339ffe987d6c1eb8
SGlinkau <97282786+SGlinkau@users.noreply.github.com>
2023-08-07 21:07:24 +0200

    First upload
    Start ;-;

---
+++ src/index.js
@@ -0,0 +1,5 @@
+import axios from "axios";
+
+axios.defaults.headers.common["live_JsN6r1FxxYTYJD5FiAycR2AgBeRInhCIqXuLzV1LCMjPijnKU2rVJJpDOjTfhmVg"] = "My key";
+
+GET https://api.thecatapi.com/v1/breeds;
